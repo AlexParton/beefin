f23cb2d8f8a33d9c17ed2b0a78442a2b2908b696
AlexParton <43645696+AlexParton@users.noreply.github.com>
2022-09-14 22:21:04 +0200

    Update firebase.js

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app';
 
 const MyFirebase = () => {
     const firebaseConfig = {
-        apiKey: "AIzaSyDZC-mAa9MBRpeDSQeTj4zE-k4WX2OpRN8",
-        authDomain: "beefin-b940a.firebaseapp.com",
-        databaseURL: "https://beefin-b940a-default-rtdb.europe-west1.firebasedatabase.app",
-        projectId: "beefin-b940a",
-        storageBucket: "beefin-b940a.appspot.com",
-        messagingSenderId: "79012311335",
-        appId: "1:79012311335:web:01da5f3aed4a1b49bcf902",
-        measurementId: "G-T24ZH4EV0R"
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: ""
       };    
       
     return initializeApp(firebaseConfig);
